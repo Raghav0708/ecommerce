7b11df3f1ad4e2ba257074c477ada2e0a3c140b1
Raghav0708 <raghavsharma820@gmail.com>
2021-11-01 16:04:30 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+
+// MDB Angular Free
+import {
+  CheckboxModule,
+  WavesModule,
+  ButtonsModule,
+  InputsModule,
+  IconsModule,
+  CardsModule,
+} from 'angular-bootstrap-md';
+
+// Angular Forms Modules
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CheckboxModule,
+    WavesModule,
+    ButtonsModule,
+    InputsModule,
+    IconsModule,
+    CardsModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
